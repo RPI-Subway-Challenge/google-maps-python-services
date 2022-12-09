29af3504355be0c304bdfd76aa831a2450426d66
lukecheun <67024671+lukecheun@users.noreply.github.com>
2022-12-09 16:43:17 -0500

    Added more test cases for stations
    Implemented a sample array of stations to test out. You can manually validate the times by putting them into maps.google.com, if necessary

--- test_distance_matrix.py
+++ test_distance_matrix.py
@@ -1,5 +1,9 @@
 """
-This file focuses primarily on using the Distance Matrix API.
+This file focuses primarily on using the Distance Matrix API. There is currently
+no compatibility with the 'subwayChallenge' or 'dataProcessing' repositories.
+Therefore, this file works INDEPENDENTLY and can be run on its own (along with API key
+text file).
+
 Refer to the link below for details and an overview of how it works.
 https://developers.google.com/maps/documentation/distance-matrix/overview
 
@@ -26,13 +30,13 @@ def address_walking_time(addressA, addressB):
     api_key = api_file.readline()
     api_file.close()
 
-    # Starting address
+    # Starting address parsing
     starting_point = addressA.replace(" ", "+")
 
-    # Destination address
+    # Destination address parsing
     destination_point = addressB.replace(" ", "+")
 
-    # Base URL for accessing distance matrix API. Already set to walking directions
+    # Base URL for accessing distance matrix API. Already set to walking directions!
     url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&mode=walking&"
     url_test = url + "origins=" + starting_point + "&destinations=" + destination_point + "&key=" + api_key
     print(url_test)
@@ -52,6 +56,13 @@ def address_walking_time(addressA, addressB):
 
 """
 Given two stations, computes the walking time between them.
+The station input should be given by its default name
+    (i.e. "Astor Pl", "Canal St")
+
+@input:     stationA        - starting station
+            stationB        - destination station
+@output:    URL link to the JSON-formatted result
+@return:    walking_time    - Time in minutes it takes to walk between the two stations
 """
 def station_walking_time(stationA, stationB):
     # Securely read the API key
@@ -59,15 +70,15 @@ def station_walking_time(stationA, stationB):
     api_key = api_file.readline()
     api_file.close()
 
-    # Starting address
-    starting_point = stationA + " station"
-    starting_point = stationA.replace(" ", "+")
+    # Starting address parsing
+    starting_point = stationA + " station new york"
+    starting_point = starting_point.replace(" ", "+")
 
-    # Destination address
-    destination_point = stationB + " station"
-    destination_point= stationB.replace(" ", "+")
+    # Destination address parsing
+    destination_point = stationB + " station subway new york"
+    destination_point= destination_point.replace(" ", "+")
 
-    # Base URL for accessing distance matrix API. Already set to walking directions
+    # Base URL for accessing distance matrix API. Already set to walking directions!
     url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&mode=walking&"
     url_test = url + "origins=" + starting_point + "&destinations=" + destination_point + "&key=" + api_key
     print(url_test)
@@ -95,7 +106,12 @@ def main():
                                                         "124 4th St, Troy, NY 12180")
 
     ### STATION TEST CASES ###
-    station_walking_time_test_A = station_walking_time("Astor Pl",
-                                                        "Canal St")
+    stations = ["Astor Pl", "Canal St", "50th St", "Bergen St", "Pennsylvania Ave",
+                "238th St", "Cathedral Pkwy", "Kingston - Throop Aves", "65th St",
+                "36th St", "Delancey St - Essex St", "Van Siclen Ave", "Norwood Ave"]
+    for i in range(0,len(stations)-1):
+        station_walking_time(stations[i], stations[i+1])
+
+
 
 main()
